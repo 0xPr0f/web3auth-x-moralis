357437de4df19e40525980be5072019ef4897487
Qudusayo <qqudusayo@gmail.com>
2022-04-12 14:43:35 +0100

    Improved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import logo from "./logo.svg";
+import w3authlogo from "./web3authlogo.png";
 import { useMoralis } from "react-moralis";
 
 function App() {
@@ -7,90 +9,61 @@ function App() {
     authError,
     isAuthenticating,
     logout,
-    Moralis,
     user,
+    authenticate,
   } = useMoralis();
 
   const handleCustomLogin = async () => {
-    await Moralis.authenticate({
+    authenticate({
       provider: "web3Auth",
       clientId:
-        "BCCTJda96ze//somefewwordsthatarehereastheclientididkwhyitisverylongwithnospacehopethisshouldhelpB394XOwT3Nr4",
+        "BGny-kghxFcbCNbfNFn1t66jwqxtnr2744vQ9aHHr06u7jhe9iI1nm1zPKLkVKer0cW2F6Weip8FZ24rwfMwFEs",
     });
   };
 
   return (
-    <>
-      {!isAuthenticated ? (
-        <div className="backgroundParent">
-          <div className="App">
-            <div>Test web3Auth x Moralis</div>
-            <div>
-              {isAuthenticating ? (
-                <p className="green"> It seems to be working</p>
-              ) : (
-                ""
-              )}
-              {authError && (
-                <p className="error">{JSON.stringify(authError.message)}</p>
-              )}
-            </div>
-            <div className="buttonCard">
-              <button className="loginButton" onClick={handleCustomLogin}>
-                Connect to web3Auth
-              </button>
-            </div>
-          </div>
-          <div>
-            For some reason, styling was hard, bear with me
-            <div>
-              <p>If you sign in or sign out, reload the page</p>
-            </div>
-            <div>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/0xPr0f/web3auth"
-              >
-                {" "}
-                Link to the github
-              </a>
-            </div>
-          </div>
+    <div className="backgroundParent">
+      <div className="App">
+        <h2>Test web3Auth x Moralis</h2>
+        <div className="logos">
+          <img src={logo} alt="moralis logo" />
+          <img src={w3authlogo} alt="web3Auth logo" />
         </div>
-      ) : (
-        <div className="backgroundParent">
-          <div className="App">
-            <div>Welcome to your account</div>
-            <div className="">
+        <div>
+          {isAuthenticating && <p className="green"> Authenticating ....</p>}
+          {authError && (
+            <p className="error">{JSON.stringify(authError.message)}</p>
+          )}
+          {isAuthenticated && (
+            <div className="accountDetails">
               <h5>Account</h5>
               <p>{user?.attributes.accounts}</p>
             </div>
-            <div className="buttonCard">
-              <button className="loginButton" onClick={logout}>
-                Sign Out
-              </button>
-            </div>
-          </div>
-          <div>
-            For some reason, styling was hard, bear with me
-            <div>
-              <p>If you sign in or sign out, reload the page</p>
-            </div>
-            <div>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/0xPr0f/web3auth"
-              >
-                {" "}
-                Link to the github
-              </a>
-            </div>
-          </div>
+          )}
         </div>
-      )}
-    </>
+        <div className="buttonCard">
+          <button
+            className="loginButton"
+            onClick={isAuthenticated ? logout : handleCustomLogin}
+          >
+            {isAuthenticated ? "Sign Out" : "Connect to web3Auth"}
+          </button>
+        </div>
+      </div>
+      <div>
+        For some reason, styling was hard, bear with me
+        <div>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/0xPr0f/web3auth"
+          >
+            {" "}
+            Link to the github
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
